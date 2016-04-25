984f55779ef6de7d9e549c69ef6517587dae49a5
Cesar Barscevicius <cesarbars@me.com>
2016-04-25 00:02:11 -0300

    Inital project setup

---
+++ Rigel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Rigel
+//
+//  Created by Cesar Barscevicius on 4/24/16.
+//  Copyright © 2016 Cesar Barscevicius. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
