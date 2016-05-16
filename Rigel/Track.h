426d13b89e045110f07b4f94e7d56199053d1e1d
Cesar Barscevicius <cesarbars@me.com>
2016-05-16 17:08:15 -0300

    Creates track representation

---
+++ Rigel/Track.h
@@ -0,0 +1,18 @@
+//
+//  Track.h
+//  Rigel
+//
+//  Created by Cesar Barscevicius on 5/16/16.
+//  Copyright © 2016 Cesar Barscevicius. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Track : NSObject
+
+@property (nonatomic, copy, readonly) NSString *title;
+@property (nonatomic, getter=isLocal, readonly) BOOL localAvailable;
+
+- (instancetype)initWithTrackTitle:(NSString *)title NS_DESIGNATED_INITIALIZER;
+
+@end
---
+++ Rigel/Track.m
@@ -0,0 +1,41 @@
+//
+//  Track.m
+//  Rigel
+//
+//  Created by Cesar Barscevicius on 5/16/16.
+//  Copyright © 2016 Cesar Barscevicius. All rights reserved.
+//
+
+#import "Track.h"
+#import "DownloadFileManager.h"
+
+@interface Track ()
+
+@property (nonatomic, copy, readwrite) NSString *title;
+@property (nonatomic, copy) NSString *filePath;
+
+@end
+
+@implementation Track
+
+- (instancetype)init {
+    self = [self initWithTrackTitle:nil];
+    return self;
+}
+
+- (instancetype)initWithTrackTitle:(NSString *)title {
+    if (self = [super init]) {
+        _title = title;
+        NSString *documentsPath = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES)[0];
+        NSString *path = [documentsPath stringByAppendingPathComponent:[NSString stringWithFormat:@"%@.mp3", _title]];
+        _filePath = path;
+    }
+
+    return self;
+}
+
+- (BOOL)isLocal {
+    return [[DownloadFileManager sharedInstance] fileExistsAtURL:[NSURL URLWithString:self.filePath]];
+}
+
+@end
